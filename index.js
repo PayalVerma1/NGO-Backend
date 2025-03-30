5291ef3d994ea5dac1a68e9fd4fd551b80c54366
PayalVerma1 <payalvermaseema@gmail.com>
2025-03-30 23:44:00 +0530

    Config changed

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 
-app.use("/api/certificate",certificateRoutes);
-app.use("/api", registerRoute);
-app.use("/api/blood",User );
+app.use("/certificate",certificateRoutes);
+app.use("/", registerRoute);
+app.use("/blood",User );
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
